34371fe377ea5bb3c215b0fb6f86b3d08606608c
billy <abongilebilly990301@gmail.com>
2024-02-20 23:12:12 +0200

    Investor controller added and mappings working

--- src/main/java/com/enviro/assessment/grad001/abongilebilly/PaperlessWithdrawals/controllers/InvestorController.java
+++ src/main/java/com/enviro/assessment/grad001/abongilebilly/PaperlessWithdrawals/controllers/InvestorController.java
@@ -1,13 +1,41 @@
 package com.enviro.assessment.grad001.abongilebilly.PaperlessWithdrawals.controllers;
 
-import com.enviro.assessment.grad001.abongilebilly.PaperlessWithdrawals.services.InvestorService;
+import com.enviro.assessment.grad001.abongilebilly.PaperlessWithdrawals.models.Investor;
+import com.enviro.assessment.grad001.abongilebilly.PaperlessWithdrawals.repositories.InvestorRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Controller
+@RestController
 public class InvestorController {
-    private InvestorService investorService;
+
+    @Autowired
+    private InvestorRepository investorRepository;
+
+
+    @GetMapping("/investors")
+    public List<Investor> getInvestors()
+    {
+
+        return investorRepository.findAll();
+    }
+
+    @PostMapping("/investors")
+    public Investor addInvestor(
+            @RequestBody Investor investor
+    )
+    {
+        return investorRepository.save(investor);
+    }
+
+
 }
